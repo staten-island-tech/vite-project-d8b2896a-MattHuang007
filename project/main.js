dc1582b29a7d689b146d725141c49ac92bb3fd73
MattHuang007 <144476342+MattHuang007@users.noreply.github.com>
2023-11-21 12:57:45 -0500

    Update main.js

--- project/main.js
+++ project/main.js
@@ -1,1 +1,62 @@
+const exotics = 
+[{
+    name:"Edge of Action",
+    class: "Titan",
+    rarity: "Exotic",
+    slot: "Energy",
+    type:"Glaive",
+image:"Add later"
+},
+{
+name:"Conditional Finality",
+class:"all",
+rarity: "Exotic",
+slot: "Kinetic",
+type: "Shotgun",
+image: "Add later",
+},
+{
+    name:"Rat King",
+    class:"all",
+    rarity: "Exotic",
+    slot: "kinetic",
+    type:"sidearm",
+    image:"Add later",
+},
+{
+name: "Heartshadow",
+class: "all",
+rarity: "Exotic",
+slot:"heavy",
+type: "sword",
+image: "Add later",
+},
+{
+name: "Other Half",
+class: "all",
+rarity:"legendary",
+slot:"heavy",
+type: "sword",
+image:"Add later",
+},
+{
+name:"Crown Splitter",
+class:"Titan",
+rarity:"legendary",
+slot:"heavy",
+type:"sword",
+image:"Add later",
+},
+{
+    name:"Edge of Intent",
+    class:"Warlock",
+    rarity:"Exotic",
+    slot:"Energy",
+    type:"Glaive",
+    image:"Add later",
+},
+{
+    name:""
+}
+]
 
